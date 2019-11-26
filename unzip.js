526f474e146c3819cce6b17820a94709423484a9
Jr Reyes <jr.evilboss@gmail.com>
2019-11-26 19:44:12 +0800

    add basic packing and extracting

--- unzip.js
+++ unzip.js
@@ -4,25 +4,59 @@ const cwd = process.cwd();
 const name = '/storage/zip/pipe.zip';
 const to = cwd + '/storage/zip/pipe-io';
 const from = path.join(cwd, name);
-console.log(to, from);
-const extract = onezip.extract(from, to);
+console.log(to, from, onezip);
 
-extract.on('file', (name) => {
-	console.log(name);
-});
+// const extract = onezip.extract(from, to);
+class extractZip {
 
-extract.on('start', (percent) => {
-	console.log('extracting started');
-});
+	unzip() {
+		extract.on('file', (name) => {
+			console.log(name);
+		});
 
-extract.on('progress', (percent) => {
-	console.log(percent + '%');
-});
+		extract.on('start', (percent) => {
+			// set status to
+			console.log('extracting started');
+		});
 
-extract.on('error', (error) => {
-	console.error(error);
-});
+		extract.on('progress', (percent) => {
+			console.log(percent + '%');
+		});
 
-extract.on('end', () => {
-	console.log('done');
-});
+		extract.on('error', (error) => {
+			console.error(error);
+		});
+
+		extract.on('end', () => {
+			console.log('done');
+		});
+	}
+
+	zip() {
+		const pack = onezip.pack(from, to, [
+			'LICENSE',
+			'README.md',
+			'package.json'
+		]);
+		pack.on('file', (name) => {
+			console.log(name);
+		});
+
+		pack.on('start', () => {
+			console.log('start packing');
+		});
+
+		pack.on('progress', (percent) => {
+			console.log(percent + '%');
+		});
+
+		pack.on('error', (error) => {
+			console.error(error);
+		});
+
+		pack.on('end', () => {
+			console.log('done');
+		});
+	}
+}
+
