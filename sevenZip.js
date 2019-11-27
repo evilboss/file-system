f693f50d01383b505adcca2b724c2ab45ee411ec
Jr Reyes <jr.evilboss@gmail.com>
2019-11-27 19:47:45 +0800

    add 7z extraction

---
+++ sevenZip.js
@@ -0,0 +1,23 @@
+const Seven = require('node-7z');
+
+// myStream is an Readable stream
+const myStream = Seven.extractFull('./storage/7z/sample.7z', './storage/7z/', {
+	$progress: true
+});
+
+myStream.on('data', function (data) {
+	console.log(data)
+});
+
+myStream.on('progress', function (progress) {
+	console.log(progress);
+});
+
+myStream.on('end', function () {
+	// end of the operation, get the number of folders involved in the operation
+	myStream.info.get('Folders');//? '4'
+	console.log('end')
+});
+
+myStream.on('error', (err) => console.error(err));
+
