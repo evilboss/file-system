b5e0c6d5d698416535c70e46078169c8fbd15954
Jr Reyes <jr.evilboss@gmail.com>
2019-12-03 22:00:51 +0800

    display stdout

--- src/unpackMock.js
+++ src/unpackMock.js
@@ -2,15 +2,17 @@ const ua = require('all-unpacker');
 const fs = require('fs');
 const _ = require('lodash');
 
-const verifyFiles = (target) => {
+const verifyFiles = (target, outputDir) => {
 	listAll(target).then((result) => {
-		checkFiles('./storage/tar/extracted/tar')
+		checkFiles(getOutputDir(target, outputDir))
 			.then((files) => {
 				const extracted = _.intersection(result, files);
 				if (result.length > extracted.length) {
 					// TODO: get unextracted files list
 					const missingFiles = _.difference(result, extracted);
-					console.log(missingFiles);
+					// console.log('files were missing', missingFiles);
+				} else {
+					console.log('no missing files');
 				}
 			});
 	});
@@ -24,14 +26,23 @@ const checkFiles = (target) => {
 
 
 };
+const getOutputDir = (target, outputDir) => {
+	let folderPath = _.last(target.split('/'));
+	folderPath = folderPath.substring(0, folderPath.indexOf('.'));
+	console.log(`${outputDir}${folderPath}`);
+	return `${outputDir}${folderPath}`;
+};
 
 const upnackAll = (target, output) => {
 	return new Promise((resolve, reject) => {
 		ua.unpack(target, {
 			targetDir: output,
+			noRecursion: true,
 			forceOverwrite: true
 		}, (err, files, info) => {
+			console.table(info);
 			if (err) reject(err);
+
 			if (files || info) resolve(files, info);
 		});
 	});
@@ -40,9 +51,13 @@ const upnackAll = (target, output) => {
 const listAll = (target) => {
 	return new Promise((resolve, reject) => {
 		ua.list(target, {}, (error, files) => {
+			console.table(files);
 			(error) ? reject(error) : resolve(_.drop(files));
 		})
 	})
+};
+const processFile = (target, outputDir) => {
+
 };
 // Usage
 /*upnackAll('./storage/tar/tar.tar', './storage/tar/extracted')
@@ -53,4 +68,4 @@ const listAll = (target) => {
 });*/
 
 
-verifyFiles('./storage/tar/tar.tar');
+upnackAll('./storage/rar/Invoice.rar', './storage/rar/');
