863a125dd351f1991bec5c2df58ba59d8e58caf6
Jr Reyes <jr.evilboss@gmail.com>
2020-01-09 16:57:30 +0800

    add dependencies to processing.js

--- processing.js
+++ processing.js
@@ -1,3 +1,30 @@
+const _ = require('lodash');
+
 const {listAll, unpackOne} = require('./extraction');
 const {convert} = require('./conversion');
 const {getFilename, getFileExtension} = require('./filename');
+const {supportedFileFormats, supportedArchives} = require('./supportedfiles.json');
+
+const isSupported = (ext) => {
+	/*TODO: return don't convert if files are [jpg, psd,png,pdf]*/
+	const defaultFormats = ['jpg', 'psd', 'png', 'pdf'];
+	return (_.includes(supportedArchives, ext)) ?
+		getOperation('extract') :
+		(_.includes(supportedFileFormats, ext)) ?
+			(_.includes(defaultFormats, ext)) ?
+				getOperation('dontConvert') :
+				getOperation('convert', ext) :
+			getOperation('unsupported');
+};
+
+const decideFileProcess = (target) => {
+	return new Promise(((resolve, reject) => {
+		const operation = isSupported(getFileExtension(target));
+		if (operation !== 'unsupported file') {
+			resolve(operation);
+		} else {
+			reject('unsupported file');
+		}
+
+	}))
+};
