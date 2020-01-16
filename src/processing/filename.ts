56346d82cb393898b7b4403bcbe2c9f499eb8c4d
Jr Reyes <jr.evilboss@gmail.com>
2020-01-16 16:23:26 +0800

    ts-ignore filename.ts warnings

--- src/processing/filename.ts
+++ src/processing/filename.ts
@@ -7,63 +7,65 @@ const {supportedFileFormats, supportedArchives} = require(`${process.env.PWD}/su
 const {getOperation} = require('./operations');
 // @ts-ignore
 const getFileExtension = (filename) => {
-  return filename.split('.').pop();
+    return filename.split('.').pop();
 };
 // @ts-ignore
 const getFilename = (filename) => {
-  return filename.split('.').slice(0, -1).join('.').replace('./testStorage', '').replace('./storage', '').replace('./storage/convert', '');
+    return filename.split('.').slice(0, -1).join('.').replace('./testStorage', '').replace('./storage', '').replace('./storage/convert', '');
 };
 
 // @ts-ignore
 const getFile = (filename) => {
-  return filename.replace(/^.*[\\\/]/, '');
+    return filename.replace(/^.*[\\\/]/, '');
 
 };
 // @ts-ignore
 const generatefileName = (file, accountName) => {
-  return `${accountName}/${file.replace(/\//g, '_').replace(/ /g, '').trim()}`;
+    return `${accountName}/${file.replace(/\//g, '_').replace(/ /g, '').trim()}`;
 
 };
 // @ts-ignore
 const renameFile = (file, account) => {
-  return (`${generatefileName(getFilename(file).replace('/convert/', '').replace('/extract/', ''), account)}.${getFileExtension(file)}`);
+    return (`${generatefileName(getFilename(file).replace('/convert/', '').replace('/extract/', ''), account)}.${getFileExtension(file)}`);
 };
 
 // @ts-ignore
 const isDefaultFormats = (ext) => {
-  const defaultFormats = ['jpg', 'psd', 'png', 'pdf'];
-  return (_.includes(defaultFormats, ext.toLowerCase()))
+    const defaultFormats = ['jpg', 'psd', 'png', 'pdf'];
+    return (_.includes(defaultFormats, ext.toLowerCase()))
 };
 // @ts-ignore
 const isSupportedFileFormats = (ext) => {
-  return _.includes(supportedFileFormats, ext.toLowerCase());
+    return _.includes(supportedFileFormats, ext.toLowerCase());
 };
 // @ts-ignore
 const isSupportedArchive = (ext) => {
-  return (_.includes(supportedArchives, ext.toLowerCase()));
+    return (_.includes(supportedArchives, ext.toLowerCase()));
 };
 // @ts-ignore
 const isSupported = (ext) => {
-  /*TODO: return don't convert if files are [jpg, psd,png,pdf]*/
-  return (isSupportedArchive(ext)) ?
-    getOperation('extract') :
-    (isSupportedFileFormats(ext)) ?
-      (isDefaultFormats(ext)) ?
-        getOperation('dontConvert') :
-        getOperation('convert', ext) :
-      getOperation('unsupported');
+    /*TODO: return don't convert if files are [jpg, psd,png,pdf]*/
+    return (isSupportedArchive(ext)) ?
+        getOperation('extract') :
+        (isSupportedFileFormats(ext)) ?
+            (isDefaultFormats(ext)) ?
+                getOperation('dontConvert') :
+                getOperation('convert', ext) :
+            getOperation('unsupported');
 };
 // @ts-ignore
 const getRealFilename = (filename) => {
-  const fileNames = filename.split('_incomingfiles-service_storage_');
-  return fileNames[1];
+    const fileNames = filename.split('_incomingfiles-service_storage_');
+    return fileNames[1];
 };
+// @ts-ignore
+
 module.exports = {
-  getFilename,
-  getFileExtension,
-  isSupported,
-  generatefileName,
-  renameFile,
-  getFile,
-  getRealFilename
+    getFilename,
+    getFileExtension,
+    isSupported,
+    generatefileName,
+    renameFile,
+    getFile,
+    getRealFilename
 };
