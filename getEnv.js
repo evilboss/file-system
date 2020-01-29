b709b92a6e40f3dbf32db943a834f75bd803d2ac
Jr Reyes <jr.evilboss@gmail.com>
2020-01-29 14:08:57 +0800

    add display AWS KEY to test

--- getEnv.js
+++ getEnv.js
@@ -1,3 +1,3 @@
 const config = require('./appConfig');
-console.log(process.env.OUTGOING_FOLDER, process.env.MONGNO_URL, process.env.IMAGINARY_UPLOAD_URL);
+console.log(process.env.OUTGOING_FOLDER, process.env.MONGNO_URL, process.env.IMAGINARY_UPLOAD_URL,process.env.AWS_KEY);
 
