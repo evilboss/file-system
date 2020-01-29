d289b524b6cfe4c401c5504064e7e1c0b1df993b
Jr Reyes <jr.evilboss@gmail.com>
2020-01-29 15:52:45 +0800

    add mongo test connection script

--- monogtester.js
+++ monogtester.js
@@ -1,2 +1,28 @@
 const {MongoClient} = require('mongodb');
-const client = new MongoClient('mongodb://localhost:27017');
+
+async function listDatabases(client) {
+	databasesList = await client.db().admin().listDatabases();
+
+	console.log("Databases:");
+	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+
+async function main() {
+	const client = new MongoClient('mongodb://localhost:27017');
+
+	try {
+		// Connect to the MongoDB cluster
+		await client.connect();
+
+		// Make the appropriate DB calls
+		await listDatabases(client);
+
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await client.close();
+	}
+}
+
+main().catch(console.error);
