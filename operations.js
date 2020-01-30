0d364e0d2189f27bf9d777a5b97235a62fe0a105
Jr Reyes <jr.evilboss@gmail.com>
2020-01-30 18:43:56 +0800

    add suppressions on operation.js

--- operations.js
+++ operations.js
@@ -1,15 +1,21 @@
+// @ts-ignore
 const _ = require('lodash');
+// @ts-ignore
 const {conversion} = require('./conversion');
+// @ts-ignore
 const {getFilename, getFileExtension, isSupported, generatefileName, renameFile} = require('./filename');
+// @ts-ignore
 const {extractFiles} = require('./extraction');
 const {OUTGOING_FOLDER} = process.env;
 const fileOperations = {
+	// @ts-ignore
 	extract: (file, accountName) => {
 		return new Promise(((resolve, reject) => {
 			extractFiles(file, accountName);
 			resolve('ok');
 		}));
 	},
+	// @ts-ignore
 	convert: (file) => {
 		return new Promise(((resolve, reject) => {
 			conversion(file).then(result => {
@@ -19,17 +25,20 @@ const fileOperations = {
 			})
 		}));
 	},
+	// @ts-ignore
 	dontConvert: (file) => {
 		return new Promise(
 			(resolve) => {
 				resolve({filename: file, imaginary: true});
 			})
 	},
+	// @ts-ignore
 	uploadToFurtherProcessing: (file) => {
 		return new Promise(resolve => {
 			resolve({filename: file, folder: OUTGOING_FOLDER})
 		})
 	},
+	// @ts-ignore
 	unsupported: (file) => {
 		return new Promise(
 			(resolve) => {
@@ -38,8 +47,9 @@ const fileOperations = {
 	}
 
 };
-
+// @ts-ignore
 const getOperation = (ext) => {
+	// @ts-ignore
 	return fileOperations[ext];
 };
 module.exports = {getOperation};
