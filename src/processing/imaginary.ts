18524eac815755167891c1a8db64d69f72a7d562
Jr Reyes <jr.evilboss@gmail.com>
2020-01-30 19:13:45 +0800

    add new file imaginary.ts

---
+++ src/processing/imaginary.ts
@@ -0,0 +1,44 @@
+// @ts-ignore
+const request = require("request");
+// @ts-ignore
+const fs = require("fs");
+// @ts-ignore
+const {NODE_ENV, IMAGINARY_UPLOAD_URL} = process.env;
+
+
+const uploadFile = (file, filename) => {
+    // @ts-ignore
+    return new Promise((resolve, reject) => {
+
+        const options = {
+            method: 'POST',
+            url: IMAGINARY_UPLOAD_URL,
+            formData:
+                {
+                    file:
+                        {
+                            value: fs.createReadStream(file),
+                            options:
+                                {
+                                    filename: filename,
+                                }
+                        }
+                }
+        };
+        console.log(options);
+        if (NODE_ENV !== 'local') {
+            request(options, (error, response, body) => {
+                console.log(body);
+
+
+            });
+        }
+    });
+};
+
+// @ts-ignore
+module.exports = {
+    imaginary: {
+        uploadFile
+    }
+};
