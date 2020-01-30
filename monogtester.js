7057e15235e83ef1855a07645c667d8419be0662
Jr Reyes <jr.evilboss@gmail.com>
2020-01-30 14:14:35 +0800

    adding diffrent commands on run

--- monogtester.js
+++ monogtester.js
@@ -1,17 +1,19 @@
 const config = require('./appConfig');
 const {MongoClient} = require('mongodb');
-const {MONGNO_URL} = process.env;
+const {MONGO_URL, MONGO_DB_NAME, PACIFIC_SERVICE_HOST} = process.env;
 
 async function listDatabases(client) {
 	databasesList = await client.db().admin().listDatabases();
 
 	console.log("Databases:");
 	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+}
 
 
 async function main() {
-	const client = new MongoClient(MONGNO_URL, {useUnifiedTopology: true});
+	console.log(`Mongo url ${MONGO_URL}/${MONGO_DB_NAME}`);
+	console.log(`pacific service host ${PACIFIC_SERVICE_HOST}`);
+	const client = new MongoClient(`${MONGO_URL}/${MONGO_DB_NAME}`, {useUnifiedTopology: true});
 
 	try {
 		// Connect to the MongoDB cluster
