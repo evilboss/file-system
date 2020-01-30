5e3d86f81807ba668d89995a58bd276cd76b487e
Jr Reyes <jr.evilboss@gmail.com>
2020-01-30 15:16:49 +0800

    added list collections in mongotester

--- monogtester.js
+++ monogtester.js
@@ -9,21 +9,30 @@ async function listDatabases(client) {
 	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 }
 
+async function listCollections(client) {
+	client.listCollections().toArray((err, collections) => {
+		console.log("collections");
+		console.log(collections, err);
+	});
+
+}
+
 
 async function main() {
 	console.log(`Mongo url ${MONGO_URL}/${MONGO_DB_NAME}`);
 	console.log(`pacific service host ${PACIFIC_SERVICE_HOST}`);
-	const client = new MongoClient(`${MONGO_URL}/${MONGO_DB_NAME}`, {useUnifiedTopology: true});
+	const client = new MongoClient(`${MONGO_URL}/harvester_test`, {useUnifiedTopology: true});
 
 	try {
 		// Connect to the MongoDB cluster
 		await client.connect();
 
 		// Make the appropriate DB calls
-		await listDatabases(client);
+		await listCollections(client);
 
 	} catch (e) {
-		console.error(e);
+		console.error('Error', e);
+
 	} finally {
 		await client.close();
 	}
