9173b30d4cfbcce8735e3505c1fec553f779d0e9
Jr Reyes <jr.evilboss@gmail.com>
2020-02-04 13:07:12 +0800

    add test for generate filename test with multiple dot files

--- filename.spec.js
+++ filename.spec.js
@@ -29,5 +29,16 @@ describe('filename', () => {
 
 
 	});
+	describe('generatefileName', () => {
+		it('should get generatefileName of file with multiple dot file', () => {
+			expect(generatefileName('sample.somedir.somefile.pdf','recliame4amazon')).toBe('recliame4amazon/sample.somedir.somefile.pdf');
+		});
+
+
+
+
+
+	});
+
 
 });
