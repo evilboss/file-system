53fbaaefab9364feb455206984847db36a14b16c
Jr Reyes <jr.evilboss@gmail.com>
2020-02-04 12:53:50 +0800

    add test for getfilename

--- filename.spec.js
+++ filename.spec.js
@@ -7,6 +7,15 @@ const {
 	getFile
 } = require('./filename');
 describe('filename', () => {
+	describe('getfilename', () => {
+		it('should get filename of normal file', () => {
+			expect(getFilename('file.pdf')).toBe('file');
+		});
+		it('should get filename of file with multiple dots file', () => {
+			expect(getFilename('file.sample.somefile.pdf')).toBe('file.sample.somefile');
+		});
+
+	});
 
 
 });
