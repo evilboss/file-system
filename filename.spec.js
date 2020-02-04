a9b5f670b99825d93f35689b1d4be102d1763bd6
Jr Reyes <jr.evilboss@gmail.com>
2020-02-04 12:55:01 +0800

    change method being called to getFileExtension

--- filename.spec.js
+++ filename.spec.js
@@ -18,10 +18,10 @@ describe('filename', () => {
 	});
 	describe('getFileExtension', () => {
 		it('should get getFileExtension of normal file', () => {
-			expect(getFilename('file.pdf')).toBe('pdf');
+			expect(getFileExtension('file.pdf')).toBe('pdf');
 		});
 		it('should get filename of file with multiple dots file', () => {
-			expect(getFilename('file.sample.somefile.pdf')).toBe('pdf');
+			expect(getFileExtension('file.sample.somefile.pdf')).toBe('pdf');
 		});
 
 	});
