0bfe3aa8afff0e2484a12fc3f7ef901d9c41d044
Jr Reyes <jr.evilboss@gmail.com>
2020-02-07 19:41:33 +0800

    add system todo

--- uploader.js
+++ uploader.js
@@ -18,6 +18,7 @@ const s3 = new AWS.S3({
 	accessKeyId: AWS_ACCESS_KEY_ID,
 	secretAccessKey: AWS_SECRET_ACCESS_KEY
 });
+/*TODO: check normal and other files*/
 
 // @ts-ignore
 const uploadFile = (targetFile, fileName, buketName = S3_BUCKET) => {
