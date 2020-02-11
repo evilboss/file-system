a370cae3f21c435a17879a1ee9c61f028a965eea
Jr Reyes <jr.evilboss@gmail.com>
2020-02-11 16:15:43 +0800

    update conversion code

--- src/processing/extraction.ts
+++ src/processing/extraction.ts
@@ -1,17 +1,17 @@
+/* istanbul ignore file */
 // @ts-ignore
 const _ = require('lodash');
 // @ts-ignore
-
+const fs = require('fs');
+// @ts-ignore
 const ua = require('./unar');
 // @ts-ignore
 const {uploadFile} = require('./uploader');
 // @ts-ignore
-
 const {INCOMING_SECONDARY_FOLDER} = process.env;
-const outputdir = './storage/extract/';
+const outputdir = './storage/';
 // @ts-ignore
 const listAll = (target) => {
-    // @ts-ignore
     return new Promise((resolve, reject) => {
         // @ts-ignore
         ua.list(target, {quiet: true}, (error, files) => {
@@ -20,8 +20,30 @@ const listAll = (target) => {
     })
 };
 // @ts-ignore
-const unpackOne = (target, output, file) => {
+const forEachPromise = (items, file, account, fn, context) => {
     // @ts-ignore
+    return items.reduce((promise, item) => promise.then(() => fn(item, file, account, context)), Promise.resolve());
+};
+
+// @ts-ignore
+const logItem = (item, file, account) => new Promise((resolve, reject) => {
+    const {getFileExtension, renameFile} = require('./filename');
+
+    process.nextTick(() => {
+        if (!getFileExtension(item).includes('/')) {
+            unpackOne(file, outputdir, item).then((payload) => {
+                uploadFile(payload, `${INCOMING_SECONDARY_FOLDER}/${renameFile(payload, account)}`, 'ingestion-ph-dev-secondary');
+            }).then(() => {
+                resolve();
+            }).catch(err => console.error(err));
+        } else {
+            resolve();
+        }
+
+    })
+});
+// @ts-ignore
+const unpackOne = (target, output, file) => {
     return new Promise((resolve, reject) => {
         ua.unpack(target, {
             archiveFile: target,
@@ -29,7 +51,7 @@ const unpackOne = (target, output, file) => {
             files: file,
             quiet: true,
             forceOverwrite: true,
-            // @ts-ignore
+// @ts-ignore
         }, (err, files, info) => {
             if (err) {
                 console.error(err);
@@ -47,26 +69,28 @@ const unpackOne = (target, output, file) => {
 const extractFiles = (file, account) => {
     // @ts-ignore
     const {getFileExtension, renameFile} = require('./filename');
-
-    listAll(file).then((result => {
+    return new Promise((resolve, reject) => {
         // @ts-ignore
-        _.each(result, (item, key) => {
-            if (!getFileExtension(item).includes('/')) {
-                unpackOne(file, outputdir, item).then((payload) => {
-                    uploadFile(payload, `${INCOMING_SECONDARY_FOLDER}/${renameFile(payload, account)}`, 'ingestion-ph-dev-secondary');
-                }).catch(err => console.error(err));
-            }
+        listAll(file).then((items => {
+            // @ts-ignore
+            forEachPromise(items, file, account, logItem).then((done) => {
+                // @ts-ignore
+                fs.unlink(file, err => {
+                    if (err) reject(err); else {
+                        resolve('done');
+                        console.log(`removed ${file} on local storage`);
+                    }
+                    // if no error, file has been deleted successfully
+                });
 
-        })
-    })).catch(error => console.error(error)).finally(() => {
-        // @ts-ignore
+            });
+        })).catch(error => reject(error))
+            // @ts-ignore
+            .finally((status) => {
+                console.log('finally', status);
+            });
+    });
 
-        return new Promise((resolve, reject) => {
-            resolve('ok');
-
-        })
-    })
-    ;
 };
 // @ts-ignore
 module.exports = {
