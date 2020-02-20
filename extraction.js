10fb683bb78c0bbecf2575d95a1e1c7338fe138d
Jr Reyes <jr.evilboss@gmail.com>
2020-02-20 17:51:35 +0800

    update extraction code

--- extraction.js
+++ extraction.js
@@ -2,12 +2,7 @@
 // @ts-ignore
 const _ = require('lodash');
 // @ts-ignore
-
 const fs = require('fs');
-// @ts-ignore
-
-const fse = require('fs-extra');
-
 // @ts-ignore
 const ua = require('./unar');
 // @ts-ignore
@@ -16,11 +11,10 @@ const {uploadFile} = require('./uploader');
 const {INCOMING_SECONDARY_FOLDER} = process.env;
 const outputdir = './storage/';
 // @ts-ignore
-const listAll = (target, all) => {
+const listAll = (target) => {
 	return new Promise((resolve, reject) => {
 		// @ts-ignore
 		ua.list(target, {quiet: true}, (error, files) => {
-			console.log('head', _.head(files));
 			(error) ? reject(error) : resolve(_.drop(files));
 		})
 	})
@@ -38,10 +32,10 @@ const logItem = (item, file, account) => new Promise((resolve, reject) => {
 	process.nextTick(() => {
 		if (!getFileExtension(item).includes('/')) {
 			unpackOne(file, outputdir, item).then((payload) => {
-				uploadFile(payload, `${INCOMING_SECONDARY_FOLDER}/${renameFile(payload, account)}`, 'ingestion-ph-dev-secondary').then((result) => {
-					resolve(result);
-				});
-			}).catch(err => reject(err));
+				uploadFile(payload, `${INCOMING_SECONDARY_FOLDER}/${renameFile(payload, account)}`);
+			}).then(() => {
+				resolve();
+			}).catch(err => console.error(err));
 		} else {
 			resolve();
 		}
@@ -55,9 +49,9 @@ const unpackOne = (target, output, file) => {
 			archiveFile: target,
 			targetDir: output,
 			files: file,
-			noDirectory: true,
 			quiet: true,
 			forceOverwrite: true,
+			noRecursion: true
 // @ts-ignore
 		}, (err, files, info) => {
 			if (err) {
@@ -78,41 +72,25 @@ const extractFiles = (file, account) => {
 	const {getFileExtension, renameFile} = require('./filename');
 	return new Promise((resolve, reject) => {
 		// @ts-ignore
-		fse.pathExists(file)
-			.then(exists => {
-				if (exists) {
-					listAll(file).then((items => {
-						// @ts-ignore
-						forEachPromise(items, file, account, logItem).then((done) => {
-							// @ts-ignore
-							/*fs.unlink(file, err => {
-								if (err) reject(err); else {
-									console.log(`removed ${file} on local storage`);
-								}
-								// if no error, file has been deleted successfully
-							});*/
-							fse.remove(file)
-								.then(() => {
-									console.log(`removed ${file} on local storage`);
-								})
-								.catch(err => {
-									console.error(err)
-								})
-							fse.remove(`${outputdir}/${_.last(items)}`)
-								.then(() => {
-									console.log(`success! removed ${outputdir}/${_.last(items)} directory on local storage`)
-								})
-								.catch(err => {
-									console.error(err)
-								})
+		listAll(file).then((items => {
+			// @ts-ignore
+			forEachPromise(items, file, account, logItem).then((done) => {
+				// @ts-ignore
+				fs.unlink(file, err => {
+					if (err) reject(err); else {
+						resolve('done');
+						console.table(items);
+						console.log(`removed ${file} on local storage`);
+					}
+					// if no error, file has been deleted successfully
+				});
 
-						});
-					})).catch(error => reject(error));
-
-				}
-			}) // => false
-
-
+			});
+		})).catch(error => console.error(error))
+			// @ts-ignore
+			.finally((status) => {
+				console.log('finally', status);
+			});
 	});
 
 };
